5ea00fb311cd0a9786d173bfb56e5511a392496a
Stephen Mizell <smizell@gmail.com>
2014-09-20 13:54:52 -0500

    Update version number and setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
+import hypermedia_resource
 
 setup(name='HypermediaResource',
-    version='0.1.0-beta1',
+    version=hypermedia_resource.__version__,
     description='Generic interface for hypermedia messages',
     author='Stephen Mizell',
     author_email='smizell@gmail.com',
     url='https://github.com/the-hypermedia-project/hypermedia-resource-python/',
     packages=['hypermedia_resource'],
+    license='MIT',
     test_suite="tests")
